67f178064f688a70d9f1074d657179478ecbda23
Konstantin Golub <key.offecka@runbox.com>
2016-07-24 14:06:29 -0400

    Allow subclasses to implement method call handling in a seprate method

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ As for now there is a couple of utilities that make life easier:
 
 1. Some lambda function wrappers
 
-2. And Auto.close block for Kotlin as it was decribed [here](https://discuss.kotlinlang.org/t/kotlin-needs-try-with-resources/214/#post_2)
+2. Some JRE8 methods exposed for Kotlin
+
+3. Default invocation handler for Java proxies
+
+4. Auto.close block for Kotlin as it was described [here](https://discuss.kotlinlang.org/t/kotlin-needs-try-with-resources/214/#post_2)
--- src/main/kotlin/net/ofk/kutils/BaseInvocationHandler.kt
+++ src/main/kotlin/net/ofk/kutils/BaseInvocationHandler.kt
@@ -7,6 +7,9 @@ import java.lang.reflect.Method
  * Base implementation for proxy handlers.
  */
 abstract class BaseInvocationHandler : InvocationHandler {
+  open fun doInvoke(proxy: Any, method: Method, args: Array<Any?>?): Any? =
+    throw UnsupportedOperationException(method.toGenericString())
+
   override fun invoke(proxy: Any, method: Method, args: Array<Any?>?): Any? =
     if (method.name == "hashCode") {
       hashCode() xor 32
@@ -15,6 +18,6 @@ abstract class BaseInvocationHandler : InvocationHandler {
     } else if (method.name == "equals" && args != null && args.size == 1 && method.parameterTypes[0] == Object::class.java) {
       proxy === args[0]
     } else {
-      throw UnsupportedOperationException(method.toString())
+      doInvoke(proxy, method, args)
     }
 }
